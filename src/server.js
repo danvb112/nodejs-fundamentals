51195150a58c1024eac649a22a96d7296b236b73
Daniel Bonasser <danielbonasser@gmail.com>
2023-12-16 19:45:49 -0300

    Write HTTP status code

--- src/server.js
+++ src/server.js
@@ -18,10 +18,10 @@ const server = http.createServer((req, res) => {
             name: 'John Doe',
         });
 
-        return res.end('User creation');
+        return res.writeHead(201).end();
     };
 
-    return res.end('Hello World');
+    return res.writeHead(404).end();
 });
 
 server.listen(3333);
